8458b5e1adf5e2e375770ecdfe10a6561e0e2328
Palaniappan Muthukkaruppan <palaniappan1993@gmail.com>
2020-06-05 21:41:21 -0400

    Update Matrix_Summation.py

--- Matrix_Summation.py
+++ Matrix_Summation.py
@@ -28,13 +28,13 @@ def before(after):
 
     for x in range(x_length):
         for y in range(y_length):
-            if x==0 and y==0:
+            if x==0 and y==0: #for (0,0) element just both after and before are same
                 result[x][y]=after[x][y]
-            elif y==0:
+            elif y==0: #for first column subtract current element with previous element in the same column
                 result[x][y] = after[x][y]-after[x-1][y]
-            elif x==0:
+            elif x==0: #for first row subtract current element with previous element in the same row
                 result[x][y] = after[x][y]-after[x][y-1]
-            else:
+            else: #for all other elements use the below formula
                 result[x][y] = (after[x][y]+after[x-1][y-1]) - (after[x-1][y]+after[x][y-1])
         
     return result
