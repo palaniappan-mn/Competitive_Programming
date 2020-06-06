11b1a74c48d16099074db5d9bbdcf6496bca2287
Palaniappan Muthukkaruppan <palaniappan1993@gmail.com>
2020-06-05 21:38:05 -0400

    Update Matrix_Summation.py

--- Matrix_Summation.py
+++ Matrix_Summation.py
@@ -23,20 +23,18 @@ def before(after):
     
     x_length = len(after)
     y_length = len(after[0])
-    result = [[0 for j in range(y_length)] for i in range(x_length)] #initializing the result with zeroes
-    result[0][0]=after[0][0] #copying the first (0,0) element from after to result
     
-    #we use the formula to transform elements from (1,1) through (length-1,length-1)
+    result = [[0 for j in range(y_length)] for i in range(x_length)] #initializing the result with zeroes
 
-    for x in range(1,x_length):
-        for y in range(1, y_length):
-            result[x][y] = (after[x][y]+after[x-1][y-1]) - (after[x-1][y]+after[x][y-1])
-                
-    #below code is to transform data from (0,1) through (0,n-1) and (1,0) through (n-1,0)
-    for x in range(1,x_length):
-        result[x][0] = after[x][0]-after[x-1][0]
-        
-    for y in range(1,y_length):
-        result[0][y] = after[0][y]-after[0][y-1]
+    for x in range(x_length):
+        for y in range(y_length):
+            if x==0 and y==0:
+                result[x][y]=after[x][y]
+            elif y==0:
+                result[x][y] = after[x][y]-after[x-1][y]
+            elif x==0:
+                result[x][y] = after[x][y]-after[x][y-1]
+            else:
+                result[x][y] = (after[x][y]+after[x-1][y-1]) - (after[x-1][y]+after[x][y-1])
         
     return result
