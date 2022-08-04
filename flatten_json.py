9424b9b72c4c2d53916c5021c4b3a3b4c30aa8f9
Palaniappan Muthukkaruppan <palaniappan1993@gmail.com>
2022-08-03 22:19:58 -0400

    Create flatten_json.py

---
+++ flatten_json.py
@@ -0,0 +1,28 @@
+input_json = {
+"test_id": "50",
+"test_name": "Amazon",
+"schedule": {
+    "test_interval":"30",
+    "day": "Tuesday",
+    "level2":{
+                "level1":"test"
+              }
+}
+}
+
+def flatten_json(input_json):
+    result = {}
+
+    def flatten_recursively(nested_json, prefix=''):
+        for key in nested_json:
+            if type(nested_json[key]) is not dict:
+                result[prefix+key] = nested_json[key]
+            else:
+                prefix = key+'_'
+                flatten_recursively(nested_json[key], prefix)
+
+    flatten_recursively(input_json)
+
+    return result
+
+print(flatten_json(input_json))
